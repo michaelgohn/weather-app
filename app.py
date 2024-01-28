3307d8c5e24178c8d191364712c1d45b941719d6
Michael Gohn <michaelgohn03@gmail.com>
2024-01-28 12:58:00 -0500

    Added route for weather-info page and created weather-info.html

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.get('/')
 def index():
     return render_template('index.html')
+
+@app.get('/weather-info/<city_name>')
+def weather_info(city_name):
+
+    return render_template('weather-info.html', city_name=city_name)
